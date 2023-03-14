3124b22b4054762da783bdfb76f34082d444495c
thebadone231 <chenhaoli123@gmail.com>
2023-03-14 22:32:58 +0800

    added firebase config

---
+++ Restorme/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCDjUHTpZGEztu4CCvWLc6USL7PSdc-kYU',
+  authDomain: 'restorme-cf3da.firebaseapp.com',
+  projectId: 'restorme-cf3da',
+  storageBucket: 'restorme-cf3da.appspot.com',
+  messagingSenderId: '779286084410',
+  appId: '1:779286084410:web:067bbae52c302fc743ae03',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
