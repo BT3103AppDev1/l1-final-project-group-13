d6acd62a4ce328792b5cb31add3de3563affb164
thebadone231 <chenhaoli123@gmail.com>
2023-03-20 12:55:59 +0800

    removed unncessary comments

--- Restorme/src/views/Register.vue
+++ Restorme/src/views/Register.vue
@@ -74,53 +74,6 @@ const register = () => {
 };
 </script>
 
-<!-- <script>
-import { ref } from 'vue';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  doc,
-} from 'firebase/firestore';
-import { useRouter } from 'vue-router';
-import { auth, db } from '../firebase.js';
-
-const router = useRouter();
-
-export default {
-  methods: {
-    async register() {
-      const email = document.getElementById('email1').value;
-      const password = document.getElementById('password1').value;
-      createUserWithEmailAndPassword(auth, email.value, password.value)
-        .then((data) => {
-          console.log('Successfully Registered');
-          router.push('./Home');
-        })
-        .catch((error) => {
-          console.log(error.code);
-          alert(error.message);
-        });
-      try {
-        const docRef = addDoc(collection(db, 'users'), {
-          Email: email.value,
-          Name: null,
-          Age: null,
-          Biography: null,
-          Experience: null,
-          SocialMediaLinks: null,
-        });
-        console.log('Document written with ID: ', docRef.id);
-      } catch (e) {
-        console.error('Error adding document: ', e);
-      }
-    },
-  },
-};
-</script> -->
-
 <style scoped>
 .container {
   display: flex;
