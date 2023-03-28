380c4dcec10034e721e9534627e776bfd47e229b
thebadone231 <chenhaoli123@gmail.com>
2023-03-28 14:41:27 +0800

    created containers for router, display of resume and comments

--- Restorme/src/main.js
+++ Restorme/src/main.js
@@ -3,5 +3,4 @@ import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
-
 app.use(router).mount('#app');
--- Restorme/src/views/Home.vue
+++ Restorme/src/views/Home.vue
@@ -1,10 +1,37 @@
 <template>
-  <div v-if="user">
-    <br />
-    <br />
-    <br />
-    <br />
-    <h1>This is the home page</h1>
+  <div v-if="numOfResumesUploaded">
+    <div id="homeContainer">
+      <div id="routerContainer">router</div>
+      <div id="resumeContainer">
+        <div id="listOfResumesContainer">
+          <div id="resumeHeaderContainer">
+            <div id="resumeHeader">
+              <h3>Your Resumes</h3>
+            </div>
+            <div id="uploadResumeButtonContainer">
+              <button id="uploadResumeButton">Upload Resume (.pdf)</button>
+            </div>
+          </div>
+          <div id="scrollViewOfResumesContainer">
+            <p>• Full stack software engineer</p>
+            <p>• Database administrator</p>
+            <p>• Product manager</p>
+            <p>• Data analyst</p>
+            <p>• Financial Analyst</p>
+            <p>• Data scientist</p>
+            <p>• Cloud engineer</p>
+            <p>• Cloud architect</p>
+            <p>• Frontend mobile engineer</p>
+            <p>• Frontend mobile engineer</p>
+            <p>• Frontend mobile engineer</p>
+            <p>• Frontend mobile engineer</p>
+            <p>• Frontend mobile engineer</p>
+          </div>
+        </div>
+        <div id="pdfContainer">pdf</div>
+      </div>
+      <div id="commentsContainer">comments</div>
+    </div>
   </div>
 </template>
 
@@ -16,6 +43,7 @@ export default {
   data() {
     return {
       user: false,
+      numOfResumesUploaded: 2,
     };
   },
 
@@ -31,3 +59,85 @@ export default {
   },
 };
 </script>
+
+<style>
+#homeContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
+
+#routerContainer {
+  flex: 1;
+  background-color: yellow;
+  align-self: center;
+  height: 95vh;
+  margin-top: 60px;
+}
+
+#resumeContainer {
+  flex: 2.5;
+  /* background-color: rgb(0, 255, 0); */
+  align-self: center;
+  height: 95vh;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+}
+
+#listOfResumesContainer {
+  flex: 1;
+  /* background-color: rgb(255, 0, 212); */
+  vertical-align: top;
+  display: flex;
+  flex-direction: column;
+  height: 150px;
+}
+
+#resumeHeaderContainer {
+  flex: 1;
+  /* background-color: rgb(255, 0, 0); */
+  display: flex;
+  align-items: center;
+}
+
+#resumeHeader {
+  flex: 1;
+  /* background-color: rgb(0, 255, 0); */
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-left: 20px;
+}
+
+#uploadResumeButtonContainer {
+  flex: 1;
+  /* background-color: rgb(0, 255, 0); */
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-right: 20px;
+}
+
+#scrollViewOfResumesContainer {
+  flex: 3;
+  /* background-color: rgb(255, 0, 212); */
+  margin-left: 40px;
+  overflow-y: scroll;
+}
+
+#pdfContainer {
+  flex: 3.5;
+  background-color: rgb(0, 0, 255);
+}
+
+#commentsContainer {
+  flex: 2;
+  background-color: rgb(0, 242, 255);
+  align-self: center;
+  height: 95vh;
+  margin-top: 60px;
+}
+</style>
