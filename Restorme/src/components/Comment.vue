cee599c057931f65c1323210ca284efd4f8a10fc
fionlim <e0857418@u.nus.edu>
2023-04-11 20:02:34 +0800

    Update Comment.vue

--- Restorme/src/components/Comment.vue
+++ Restorme/src/components/Comment.vue
@@ -1,25 +1,31 @@
 <template>
-  <div id="comment">
-    <div id="votesContainer">
-      <div class="vote">+</div>
-      <div class="voteCount">3</div>
-      <div class="vote">-</div>
-    </div>
-    <div id="contentContainer">
-      <div id="userDetailsContainer">test002@gmail.com</div>
-      <button id = "replyButton" v-on:click="component = 'reply-input'">
-            <div style = "padding-bottom: 10%">
-              <ReplyIcon/>
+  <div>
+    <div id="comment">
+      <div id="votesContainer">
+        <div class="vote">+</div>
+        <div class="voteCount">3</div>
+        <div class="vote">-</div>
+      </div>
+      <div id="contentContainer">
+        <div id="topHalfContent">
+          <div id="userDetailsContainer">test002@gmail.com</div>
+          <button id="replyButton" v-on:click="component = 'reply-input'">
+            <div style="padding-bottom: 10%">
+              <ReplyIcon />
             </div>
-            <p style = "margin-left: 8%">Reply</p>
+            <p style="margin-left: 8%">Reply</p>
           </button>
-      <div id="commentDetailsContainer">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat autem
-        maiores nemo, distinctio, modi consequuntur dolorem velit doloribus iure
-        illum rem voluptatum est, enim commodi cumque corporis pariatur quaerat
-        dicta.
+        </div>
+        <div id="commentDetailsContainer">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat autem
+          maiores nemo, distinctio, modi consequuntur dolorem velit doloribus iure
+          illum rem voluptatum est, enim commodi cumque corporis pariatur quaerat
+          dicta.
+        </div>
+
       </div>
     </div>
+    <component v-bind:is="component" @remove="cancelComment()" v-bind:comment_id='comment_id'></component>
   </div>
 
   <div class="container">
@@ -27,6 +33,7 @@
       <h2>Add Comments</h2>
 
       <div class="formli">
+
         <label for="commentType">Category:</label>
         <select id="category" v-model="selected">
           <option disabled value="">Please Select a Category</option>
@@ -39,21 +46,11 @@
         <br /><br />
 
         <label for="Description">Description: </label>
-        <textarea
-          id="Description"
-          rows="4"
-          cols="35"
-          placeholder="Enter your comment"
-          required
-        ></textarea>
+        <textarea id="Description" rows="4" cols="50" placeholder="Enter your comment" required></textarea>
         <br /><br />
 
         <div class="save">
-          <button
-            id="savebutton"
-            type="button"
-            v-on:click="saveCommentToFS(12345678)"
-          >
+          <button id="savebutton" type="button" v-on:click="saveCommentToFS(12345678)">
             Save
           </button>
         </div>
@@ -63,97 +60,107 @@
 </template>
 
 <script>
-import { auth } from '../firebase';
-import { db } from '../firebase';
-import { addDoc, collection, doc, setDoc, updateDoc } from 'firebase/firestore';
-import { getFirestore } from 'firebase/firestore';
+import { auth } from "../firebase";
+import { db } from "../firebase";
+import { addDoc, collection, doc, setDoc, updateDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import ReplyIcon from "../assets/ReplyIcon.vue"
+import ReplyInput from "./ReplyInput.vue";
+
+
 
 export default {
+  components: {
+    ReplyIcon,
+    "reply-input": ReplyInput
+  },
+
+  mounted() {
+    const commentCollection = collection(db, "Comment_Collection");
+  },
+
+  data() {
+    return {
+      component: null,
+      new_reply: "",
+      comment_id: "AGEeFQOAGfpxxrlEdkjl",
+      comment_user: "test002@gmail.com",
+    }
+  },
+
   methods: {
+
     async saveCommentToFS(resumeID) {
-      const commentCollection = collection(db, 'Comment_Collection');
+      const commentCollection = collection(db, "Comment_Collection")
       const user = auth.currentUser;
       const userUID = auth.currentUser.uid;
-      console.log('IN AC');
+      console.log("IN AC")
 
       /*
        * A COMMENT SHOULD HAVE
        * COMMENT_ID /
        * COMMENT_TYPE /
-       * USER_UID /
-       * RESUME_ID /
-       * DATE /
+       * USER_UID / 
+       * RESUME_ID / 
+       * DATE / 
        * DESCRIPTION (COMMENT) /
        * NO. OF UPVOTES /
        * NO. OF DOWNVOTES /
-       * MARKED USEFUL /
+       * MARKED USEFUL / 
        * REPLIES - TBC
        */
       let resume_id = resumeID;
-      let comment = document.getElementById('Description').value;
-      let category = document.getElementById('category');
-      let categoryValue = category.value;
+      let comment = document.getElementById("Description").value;
+      let category = document.getElementById("category");
+      let categoryValue = category.value
       let categoryText = category.options[category.selectedIndex].text;
 
       let currentdate = new Date();
-      let dateTime =
-        currentdate.getDate() +
-        '/' +
-        (currentdate.getMonth() + 1) +
-        '/' +
-        currentdate.getFullYear() +
-        ' @ ' +
-        currentdate.getHours() +
-        ':' +
-        currentdate.getMinutes() +
-        ':' +
-        currentdate.getSeconds();
+      let dateTime = currentdate.getDate() + "/"
+        + (currentdate.getMonth() + 1) + "/"
+        + currentdate.getFullYear() + " @ "
+        + currentdate.getHours() + ":"
+        + currentdate.getMinutes() + ":"
+        + currentdate.getSeconds();
       //let uploadDate = Math.floor(Date.now() /1000)
 
       let upvotesNum = 0;
       let downvotesNum = 0;
       let markedUseful = false;
-      let replies = 'to be confirmed';
+      let replies = "to be confirmed";
 
-      alert(
-        ' Saving your data for Comment : ' +
-          comment +
-          categoryText +
-          dateTime +
-          userUID +
-          upvotesNum +
-          downvotesNum +
-          markedUseful +
-          replies
-      );
+
+      alert(" Saving your data for Comment : " + comment + categoryText + dateTime +
+        userUID + upvotesNum + downvotesNum + markedUseful + replies)
 
       try {
-        const docRef = await addDoc(collection(db, 'Comments'), {
-          Comment_ID: '',
-          Resume_ID: resume_id,
-          Comment_Category: categoryText,
-          User: userUID,
-          Upload_Date: dateTime,
-          Description: comment,
-          Number_Of_Upvotes: upvotesNum,
-          Number_of_Downvotes: downvotesNum,
-          Marked_Useful: markedUseful,
-          Replies: replies,
-        });
+        const docRef = await addDoc(collection(db, "Comments"), {
+          Comment_ID: "", Resume_ID: resume_id, Comment_Category: categoryText, User: userUID, Upload_Date: dateTime,
+          Description: comment, Number_Of_Upvotes: upvotesNum, Number_of_Downvotes: downvotesNum,
+          Marked_Useful: markedUseful, Replies: replies
+        })
 
-        console.log(String(docRef.id));
-        const commentsRef = doc(db, 'Comments', String(docRef.id));
+        console.log(String(docRef.id))
+        const commentsRef = doc(db, "Comments", String(docRef.id))
         await updateDoc(commentsRef, {
-          Comment_ID: String(docRef.id),
-        });
+          Comment_ID: String(docRef.id)
+        })
         document.getElementById('myform').reset();
-        this.$emit('added');
-      } catch (error) {
-        console.error('Error adding document: ', error);
+        this.$emit("added")
       }
+      catch (error) {
+        console.error("Error adding document: ", error);
+      }
+
     },
-  },
-};
+
+    cancelComment() {
+      this.component = null;
+    },
+
+
+  }
+}
 /* 
 
 export default {
@@ -216,12 +223,12 @@ input:hover {
   border-radius: 2px;
 }
 
-input[type='text'] {
+input[type="text"] {
   width: 20em;
 }
 
 select {
-  width: 15em;
+  width: 20em;
 }
 
 #comment {
@@ -272,6 +279,11 @@ select {
   /* background-color: rgb(255, 0, 0); */
 }
 
+#topHalfContent {
+  display: flex;
+  flex-direction: row;
+}
+
 @font-face {
   font-family: 'Rubik-Regular';
   src: local('Rubik-Regular');
@@ -300,5 +312,17 @@ select {
   font-family: Rubik-Regular;
   font-weight: medium;
   margin-bottom: 5px;
+}
+
+#replyButton {
+  width: 25%;
+  font-size: 80%;
+  height: 50%;
+  display: flex;
+  flex-direction: row;
+  margin-top: 5%;
+  margin-right: 5%;
+  background-color: white;
+  border: none;
 }
 </style>
